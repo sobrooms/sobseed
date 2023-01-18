fbdda1c38a3f312a5ffafd7d7985e572bb1ca166
rrryfu pc 😱 <83335294+mangorifo@users.noreply.github.com>
2023-01-18 14:04:27 +0800

    change announcements and translation

--- src/main/java/emu/grasscutter/BuildConfig.java
+++ src/main/java/emu/grasscutter/BuildConfig.java
@@ -2,5 +2,5 @@ package emu.grasscutter;
 
 public final class BuildConfig {
     public static final String VERSION = "1.4.6";
-    public static final String GIT_HASH = "99de1a1";
+    public static final String GIT_HASH = "c2a8391ba";
 }
