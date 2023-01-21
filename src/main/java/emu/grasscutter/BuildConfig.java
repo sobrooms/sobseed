cde5cd8af8d43176067a7f3d0960f73db48db19d
NickTheHuy <nikurlapov@gmail.com>
2023-01-21 09:09:44 +0200

    hehe

--- src/main/java/emu/grasscutter/BuildConfig.java
+++ src/main/java/emu/grasscutter/BuildConfig.java
@@ -1,6 +1,6 @@
 package emu.grasscutter;
 
 public final class BuildConfig {
-    public static final String VERSION = "1.4.6";
-    public static final String GIT_HASH = "c13bd6997";
+    public static final String VERSION = "";
+    public static final String GIT_HASH = "34e59ff53";
 }
