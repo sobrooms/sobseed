ad47bb370964e34ca0c9527105461c4ac4b945a4
Julek <julek.zolich@gmail.com>
2023-01-22 01:59:43 +0100

    Rework windy

--- src/main/java/emu/grasscutter/server/packet/send/PacketWindSeedClientNotify.java
+++ src/main/java/emu/grasscutter/server/packet/send/PacketWindSeedClientNotify.java
@@ -1,27 +1,19 @@
 package emu.grasscutter.server.packet.send;
 
 import com.google.protobuf.ByteString;
-import emu.grasscutter.Grasscutter;
 import emu.grasscutter.net.packet.BasePacket;
+import emu.grasscutter.net.packet.PacketOpcodes;
 import emu.grasscutter.net.proto.WindSeedClientNotifyOuterClass.WindSeedClientNotify;
 import emu.grasscutter.net.proto.WindSeedClientNotifyOuterClass.WindSeedClientNotify.AreaNotify;
-import emu.grasscutter.utils.FileUtils;
 
 public class PacketWindSeedClientNotify extends BasePacket {
 
-	public PacketWindSeedClientNotify(String givenPath) {
-		super(1157);
-		byte[] data;
-		try {
-			data = FileUtils.read(FileUtils.getDataPath(givenPath));
-		} catch (Exception e) {
-			Grasscutter.getLogger().error("Error Windy: ", e);
-			return;
-		}
+	public PacketWindSeedClientNotify(ByteString data) {
+		super(PacketOpcodes.WindSeedClientNotify); //why it was hardcoded?
 		WindSeedClientNotify proto = WindSeedClientNotify
 			.newBuilder()
 			.setAreaNotify(
-				AreaNotify.newBuilder().setAreaId(1).setAreaType(1).setAreaCode(ByteString.copyFrom(data)).build()
+				AreaNotify.newBuilder().setAreaId(1).setAreaType(1).setAreaCode(data).build()
 			)
 			.build();
 		this.setData(proto);
